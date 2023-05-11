7add8e35620c5219eb51e8565e54c71c126bac7d
Ayddan <nathanleduc@hotmail.fr>
2023-05-11 18:13:37 +0200

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
+import React, { createContext, useState } from 'react'; 
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/components/Home';
+
+const Stack = createNativeStackNavigator()
+
+const UserContext = createContext({})
 
 export default function App() {
+  const [userToken, setUserToken] = useState(null)
+  const [user, setUser] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+
+  if(isLoading){
+    return(
+      <>
+        <Text>Loading...</Text>
+      </>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <UserContext.Provider value = {{userToken, user}}>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false
+          }}
+          initialRouteName="Home"
+        >
+          <Stack.Screen   
+            name="Home"
+            component={Home}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </UserContext.Provider>
   );
 }
 
